7c2d3e4fd967464bafd458aea05a409e32e2e2a8
Mikiya Fujimoto <1910370142t@kindai.ac.jp>
2021-07-12 16:42:31 +0900

    Initial Commit

---
+++ CompactTemilliusApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CompactTemilliusApp
+//
+//  Created by 藤本幹也 on 2021/07/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
